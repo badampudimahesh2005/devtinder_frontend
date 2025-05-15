c17eddd891ac9d69d97fad44cb4f5140863e156d
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-05-15 22:33:16 +0530

    landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Body from './pages/Body'
+import ContactUs from './pages/ContactUs'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <BrowserRouter basename='/'>
+
+      <Routes>
+        <Route path='/' element={<Body />} >
+         <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/terms-of-service" element={<TermsOfService />} /> */}
+          {/* <Route path="/privacy-policy" element={<PrivacyPolicy />} /> */}
+          <Route path="/contact-us" element={<ContactUs />} />
+
+          
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+
+
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Footer from "../components/Footer";
+
+
+const Home = () => {
+   return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home
